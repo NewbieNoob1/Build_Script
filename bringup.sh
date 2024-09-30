43e840082fc8fb6998d7b8a43c35f74973c07973
300 <romtesting99@gmail.com>
2024-09-30 14:48:26 +0530

    munch: added some more cmds
    updated script from prateek
    - pocket mode
    - Xiaomi Vibrator Feature

--- bringup.sh
+++ bringup.sh
@@ -1,39 +1,85 @@
 #!/bin/bash
 
+#Removing Device Resources (if any)
+echo "========================================================================"
+echo "DELETING DIRECTORIES"
+echo "========================================================================"
+
 rm -rf device/xiaomi/munch
 rm -rf device/xiaomi/sm8250-common
 rm -rf vendor/xiaomi/munch
 rm -rf vendor/xiaomi/sm8250-common
 rm -rf kernel/xiaomi/sm8250
+rm -rf vendor/xiaomi/miuicamera
 
 echo "========================================================================"
-echo "DELETED DIRECTORIES"
+echo "DELETED DIRECTORIES SUCCESSFULLY"
 echo "========================================================================"
 
-# Initiating the Rising OS
+#Initiating RisingOS
+echo "========================================================================"
+echo "INITIALIZING ROM REPOSITORY"
+echo "========================================================================"
+
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 
 echo "========================================================================"
-echo "REPO INITIALIZED"
+echo "ROM REPOSITORY INITIALIZED SUCCESSFULLY"
 echo "========================================================================"
 
-# Clone Munch Trees
-# 1. Device Tree
-git clone https://github.com/NewbieNoob1/device_xiaomi_munch.git --depth=1 -b fourteen device/xiaomi/munch
-# 2. Device Common tree
-git clone https://github.com/NewbieNoob1/device_xiaomi_sm8250-common.git --depth=1 -b fourteen device/xiaomi/sm8250-common
-# 3. Vendor Tree
+#Clone Resources
+echo "========================================================================"
+echo "CLONING BASIC MUNCH RESOURCES"
+echo "========================================================================"
+
+#1. Device Tree
+git clone https://github.com/DeadlyShroud/device_xiaomi_munch.git --depth=1 -b fourteen device/xiaomi/munch
+#2. Common Device Tree
+git clone https://github.com/DeadlyShroud/device_xiaomi_sm8250-common.git --depth=1 -b fourteen device/xiaomi/sm8250-common
+#3. Vendor Tree
 git clone https://gitea.com/deadlyshroud/vendor_xiaomi_munch.git --depth=1 -b fourteen vendor/xiaomi/munch
-# 4. Vendor Common Tree
+#4. Common Vendor Tree
 git clone https://gitea.com/deadlyshroud/vendor_xiaomi_sm8250-common.git --depth=1 -b fourteen vendor/xiaomi/sm8250-common
-# 5. Kernel Tree
+#5. Kernel Tree
 git clone https://github.com/kvsnr113/xiaomi_sm8250_kernel.git --depth=1 -b main kernel/xiaomi/sm8250
 
 echo "========================================================================"
-echo "MUNCH TREES CLONED"
+echo "BASIC MUNCH RESOURCES CLONED SUCCESSFULLY"
 echo "========================================================================"
 
-# Setup Clang toolchain
+#Extra Additions
+echo "========================================================================"
+echo "CLONING EXTRA STUFF"
+echo "========================================================================"
+
+#1. Hardware Xiaomi
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth=1 -b lineage-21 hardware/xiaomi
+
+#2. Hardware Display
+rm -rf hardware/qcom-caf/sm8250/display
+git clone https://github.com/hdzungx/android_hardware_qcom-caf_sm8250_display --depth=1 -b uqpr3 hardware/qcom-caf/sm8250/display
+
+#3. Leica Camera
+git clone https://gitea.com/hdzungx/android_vendor_xiaomi_miuicamera.git --depth=1 -b uqpr3 vendor/xiaomi/miuicamera
+
+#4. Viper4AndroidFX
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git --depth=1 -b v4a packages/apps/ViPER4AndroidFX
+
+#5. KProfiles 
+rm -rf packages/apps/KProfiles
+git clone https://github.com/yaap/packages_apps_KProfiles.git --depth=1 -b fourteen packages/apps/KProfiles
+
+echo "========================================================================"
+echo "EXTRA STUFF CLONED SUCCESSFULLY"
+echo "========================================================================"
+
+#Modifications
+echo "========================================================================"
+echo "MODIFICATIONS STARTED"
+echo "========================================================================"
+
+#1. Neutron Clang
 cd prebuilts/clang/host/linux-x86
 mkdir clang-neutron
 cd clang-neutron
@@ -43,57 +89,34 @@ chmod +x antman
 ./antman --patch=glibc
 cd ../../../../..
 
-echo "========================================================================"
-echo "CLANG-NEUTRON SETUP SUCCESS"
-echo "========================================================================"
+#2. Pocket Mode
+sed -i 's/android:minSdkVersion="19"/android:minSdkVersion="21"/' prebuilts/sdk/current/androidx/m2repository/androidx/preference/preference/1.3.0-alpha01/manifest/AndroidManifest.xml
 
-## Extra Additions
-
-# Hardware Xiaomi
-rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git --depth=1 -b lineage-21 hardware/xiaomi
-
-# Hardware Display
-rm -rf hardware/qcom-caf/sm8250/display
-git clone https://github.com/hdzungx/android_hardware_qcom-caf_sm8250_display --depth=1 hardware/qcom-caf/sm8250/display
-
-# Miui Camera Repo
-git clone https://gitea.com/hdzungx/android_vendor_xiaomi_miuicamera.git --depth=1 -b uqpr3 vendor/xiaomi/miuicamera
-
-# Viper4Android
-git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git --depth=1 -b v4a packages/apps/ViPER4AndroidFX
-
-# Kprofiles 
-rm -rf packages/apps/KProfiles
-git clone https://github.com/yaap/packages_apps_KProfiles.git --depth=1 -b fourteen packages/apps/KProfiles
+#3. XiaomiVibrator Feature
+cd frameworks/native
+git fetch https://github.com/VoidUI-Tiramisu/frameworks_native refs/heads/aosp-13 && git cherry-pick d3b4026058e9d44759860c0b69d35de3f801c4e1
+cd../../
 
 echo "========================================================================"
-echo "EXTRA ADDITIONS CLONED"
+echo "MODIFICATIONS DONE SUCCESSFULLY"
 echo "========================================================================"
 
-# Resync
+#Resync
+echo "========================================================================"
+echo "RESYNCING SOURCE"
+echo "========================================================================"
 
 /opt/crave/resync.sh
 
 echo "========================================================================"
-echo "RESYNCED"
+echo "RESYNCED SOURCE SUCCESSFULLY"
 echo "========================================================================"
 
-
-# Modify the AndroidManifest.xml
-sed -i 's/android:minSdkVersion="19"/android:minSdkVersion="21"/' prebuilts/sdk/current/androidx/m2repository/androidx/preference/preference/1.3.0-alpha01/manifest/AndroidManifest.xml
-
+#Build
 echo "========================================================================"
-echo "ANDROID MANIFEST MODIFIED"
+echo "BUILD STARTING"
 echo "========================================================================"
 
-
-echo "========================================================================"
-echo "BUILDING........."
-echo "========================================================================"
-
-
-# RISEUP
 source build/envsetup.sh
 riseup munch userdebug
 gk -s
